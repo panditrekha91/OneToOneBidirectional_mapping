4c387e6bb6317d39723c318f2df43d74f547cc4c
Rekha Pandit <panditrekha91@gmail.com>
2023-03-23 12:36:38 +0530

    second commit getmapping add in employee controller

--- src/main/java/onetooneBidirectional_mapping/controller/EmpController.java
+++ src/main/java/onetooneBidirectional_mapping/controller/EmpController.java
@@ -1,10 +1,13 @@
 package onetooneBidirectional_mapping.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,13 @@ public class EmpController {
 	{
 		Employee e=empservice.insertemp(obj);
 		return new ResponseEntity<>(e,HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public List<Employee> getAll()
+	{
+		return empservice.getAllemp();
+		
 	}
 	
 }
--- src/main/java/onetooneBidirectional_mapping/service/EmpService.java
+++ src/main/java/onetooneBidirectional_mapping/service/EmpService.java
@@ -1,5 +1,7 @@
 package onetooneBidirectional_mapping.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,10 @@ public class EmpService {
 		Employee obj=empinterface.save(e);
 		return obj;
 		
+	}
+
+	public List<Employee> getAllemp() {
+		
+		return empinterface.findAll();
 	}
 }
